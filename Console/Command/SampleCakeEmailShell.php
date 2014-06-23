3b693dfb03b85deaa62a7d60c64a986e16b88b34
Guy Warner <gwarner@blendtec.com>
2014-06-23 10:46:05 -0600

    Update Sample script to cake standards

--- Console/Command/SampleCakeEmailShell.php
+++ Console/Command/SampleCakeEmailShell.php
@@ -1,11 +1,11 @@
 <?php
 /**
- *  Sample of CakeEmail Transport for Amazon SES
+ * Sample of CakeEmail Transport for Amazon SES
  *
- *  @copyright Copyright 2012 News2u Corporation
- *  @link http://www.news2u.co.jp/
- *  @package cake
- *  @license MIT License (http://www.opensource.org/licenses/mit-license.php)
+ * @copyright Copyright 2012 News2u Corporation
+ * @link http://www.news2u.co.jp/
+ * @package cake
+ * @license MIT License (http://www.opensource.org/licenses/mit-license.php)
  *
  */
 CakePlugin::loadAll();
@@ -20,22 +20,26 @@ App::uses('CakeEmail', 'Network/Email');
  * @subpackage Console.Shell
  */
 class SampleCakeEmailShell extends AppShell {
-    public function main() {
-        $email = new CakeEmail();
-        $email->config(array(
-                'transport' => 'AmazonSESTransport.AmazonSES',
-                'log' => true,
-                'Amazon.SES.Key' => Configure::read('Amazon.SES.Key'),
-                'Amazon.SES.Secret' => Configure::read('Amazon.SES.Secret'),
-            ));
 
-        $email->sender('no-reply@example.org');
-        $email->from('no-reply@example.org', 'Example');
-        $email->to('test@example.org');
-        $email->bcc('bcc@example.org');
-        $email->subject('SES Test from CakePHP');
-
-        $res = $email->send('test message.');
-        var_dump($res);
-    }
+/**
+ * Main function for sample
+ * 
+ * @return void
+ */
+	public function main() {
+		$email = new CakeEmail();
+		$email->config(array(
+				'transport' => 'AmazonSESTransport.AmazonSES',
+				'log' => true,
+				'Amazon.SES.Key' => Configure::read('Amazon.SES.Key'),
+				'Amazon.SES.Secret' => Configure::read('Amazon.SES.Secret'),
+			));
+		$email->sender('no-reply@example.org');
+		$email->from('no-reply@example.org', 'Example');
+		$email->to('test@example.org');
+		$email->bcc('bcc@example.org');
+		$email->subject('SES Test from CakePHP');
+		$res = $email->send('test message.');
+		var_dump($res);
+	}
 }
