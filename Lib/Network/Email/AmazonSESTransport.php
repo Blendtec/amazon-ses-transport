550fc978b2e5cb2b3aa78eccc8d003f814fd5ab7
Guy Warner <gwarner@blendtec.com>
2014-06-23 13:31:16 -0600

    More code clean up, removed unused variable

--- Lib/Network/Email/AmazonSESTransport.php
+++ Lib/Network/Email/AmazonSESTransport.php
@@ -28,28 +28,24 @@ class AmazonSESTransport extends AbstractTransport {
  * @throws SocketException When mail cannot be sent.
  */
 	public function send(CakeEmail $email) {
-		$initOptions['key'] = $this->_config['Amazon.SES.Key'];
-		$initOptions['secret'] = $this->_config['Amazon.SES.Secret'];
-		$initOptions['region'] = $this->_config['Amazon.SES.Region'];
-		$ses = SesClient::factory($initOptions);
+		$ses = SesClient::factory(array(
+			'key' => $this->_config['Amazon.SES.Key'],
+			'secret' => $this->_config['Amazon.SES.Secret'],
+			'region' => $this->_config['Amazon.SES.Region']
+		));
 
 		$eol = PHP_EOL;
 		if (isset($this->_config['eol'])) {
 			$eol = $this->_config['eol'];
 		}
-
-		$option = isset($this->_config['additionalParameters']) ? $this->_config['additionalParameters'] : array();
-		$headers = $email->getHeaders(array('from', 'sender', 'replyTo', 'readReceipt', 'returnPath', 'to', 'cc', 'subject'));
-		$headers = $this->_headersToString($headers);
+		$headers = $this->_headersToString($email->getHeaders(array('from', 'sender', 'replyTo', 'readReceipt', 'returnPath', 'to', 'cc', 'subject')));
 		$message = implode($eol, (array)$email->message());
 		$rawMessage = $headers . $eol . $eol . $message;
 		$res = $ses->sendRawEmail(array('RawMessage' => array('Data' => base64_encode($rawMessage) )));
-		$b = $res->toArray();
-		$messageId = $b['MessageId'];
-
+		$results = $res->toArray();
 		if (!$res) {
 			throw new SocketException(__d('cake_dev', 'Could not send email.'));
 		}
-		return array('headers' => $headers, 'messageId' => $messageId, 'message' => $message);
+		return array('headers' => $headers, 'messageId' => $results['MessageId'], 'message' => $message);
 	}
 }
