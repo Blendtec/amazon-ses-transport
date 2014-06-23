b84d8b0e09ecc7879aa9cdfe07c1d72dac0d9b21
Guy Warner <gwarner@blendtec.com>
2014-06-23 10:43:29 -0600

    Update to CakePHP standards

--- Lib/Network/Email/AmazonSESTransport.php
+++ Lib/Network/Email/AmazonSESTransport.php
@@ -20,37 +20,36 @@ use Aws\Ses\SesClient;
  */
 class AmazonSESTransport extends AbstractTransport {
 
-    /**
-     * Send mail
-     *
-     * @param CakeEmail $email CakeEmail
-     * @return array
-     * @throws SocketException When mail cannot be sent.
-     */
+/**
+ * Send mail
+ *
+ * @param CakeEmail $email CakeEmail
+ * @return array
+ * @throws SocketException When mail cannot be sent.
+ */
 	public function send(CakeEmail $email) {
-        $init_options['key'] = $this->_config['Amazon.SES.Key'];
-        $init_options['secret'] = $this->_config['Amazon.SES.Secret'];
-        $init_options['region'] = $this->_config['Amazon.SES.Region'];
-        $ses = SesClient::factory($init_options);
+		$initOptions['key'] = $this->_config['Amazon.SES.Key'];
+		$initOptions['secret'] = $this->_config['Amazon.SES.Secret'];
+		$initOptions['region'] = $this->_config['Amazon.SES.Region'];
+		$ses = SesClient::factory($initOptions);
 
 		$eol = PHP_EOL;
 		if (isset($this->_config['eol'])) {
 			$eol = $this->_config['eol'];
 		}
 
-        $option = isset($this->_config['additionalParameters']) ? $this->_config['additionalParameters'] : array();
+		$option = isset($this->_config['additionalParameters']) ? $this->_config['additionalParameters'] : array();
 		$headers = $email->getHeaders(array('from', 'sender', 'replyTo', 'readReceipt', 'returnPath', 'to', 'cc', 'subject'));
 		$headers = $this->_headersToString($headers);
 		$message = implode($eol, (array)$email->message());
-        $raw_message = $headers . $eol . $eol . $message;
-        $res = $ses->sendRawEmail(array('RawMessage' => array('Data' => base64_encode($raw_message) )));
-        
-        $b = $res->toArray();
-        $MessageId = $b['MessageId'];
+		$rawMessage = $headers . $eol . $eol . $message;
+		$res = $ses->sendRawEmail(array('RawMessage' => array('Data' => base64_encode($rawMessage) )));
+		$b = $res->toArray();
+		$MessageId = $b['MessageId'];
 
-        if(!$res)
-            throw new SocketException(__d('cake_dev', 'Could not send email.'));
-       
-        return array('headers' => $headers, 'messageId' => $MessageId, 'message' => $message);
+		if (!$res) {
+			throw new SocketException(__d('cake_dev', 'Could not send email.'));
+		}
+		return array('headers' => $headers, 'messageId' => $MessageId, 'message' => $message);
 	}
 }
